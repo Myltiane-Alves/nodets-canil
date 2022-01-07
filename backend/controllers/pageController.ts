0cd3519d8fd5671fed844cf032fad8efb95e8395
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-07 10:08:08 -0300

    Adicionando funcionalidades nas rotas

--- backend/controllers/pageController.ts
+++ backend/controllers/pageController.ts
@@ -1,18 +1,45 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObjects';
 
 export const home = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        banner:  {
+            menu: createMenuObject('all'),
+            title: 'Todos os Animais',
+            background: 'allanimals.jpg' 
+        }
+    })
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner:  {
+            title: 'Cachorro',
+            background: 'banner_dog.jpg' 
+        }
+    })
 }
 
 export const cats = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner:  {
+            title: 'Gatos',
+            background: 'banner_cat.jpg' 
+        }
+    })
 
 }
 
 export const fishes = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner:  {
+            title: 'Peixes',
+            background: 'banner_fish.jpg' 
+        }
+    })
 
 }
 
--- backend/server.ts
+++ backend/server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const server = express();
 
 server.set('view engine', 'mustache');
-server.set('view engine', path.join(__dirname, 'views'));
+server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../frontend')));
@@ -17,7 +17,7 @@ server.use(express.static(path.join(__dirname, '../frontend')));
 server.use(mainRoutes);
 
 server.use((req, res) => {
-    res.send('página não encontrada!');
+    res.send('pages/404');
 });
 
 // Rotas
